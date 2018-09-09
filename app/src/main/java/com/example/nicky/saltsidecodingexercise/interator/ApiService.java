5fdf8e9ef2b450e0f0b5c0facea5972deb55e009
nickyk2407 <nickykumari5@gmail.com>
2018-09-10 02:11:33 +0530

    commit

--- app/src/main/java/com/example/nicky/saltsidecodingexercise/interator/ApiService.java
+++ app/src/main/java/com/example/nicky/saltsidecodingexercise/interator/ApiService.java
@@ -12,8 +12,8 @@ import retrofit2.http.GET;
  */
 
 public interface ApiService {
-    String API_ENDPOINT = "https://gist.githubusercontent.com/ashwini9241/";
+    String API_ENDPOINT = "https://gist.githubusercontent.com/";
 
-    @GET("6e0f26312ddc1e502e9d280806eed8bc/raw/7fab0cf3177f17ec4acd6a2092fc7c0f6bba9e1f/saltside-json-data")
+    @GET("ashwini9241/6e0f26312ddc1e502e9d280806eed8bc/raw/7fab0cf3177f17ec4acd6a2092fc7c0f6bba9e1f/saltside-json-data")
     Observable<List<JsonResponse>> getJsonData();
 }
